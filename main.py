647fe1038976037b058fee61503c539f71b51eb4
EmirKIRAN <emirr.kirann@gmail.com>
2021-06-01 11:21:15 +0300

    masking video with RGB color space

--- main.py
+++ main.py
@@ -8,13 +8,38 @@ base_dir = os.getcwd()
 videos_dir = os.path.join(base_dir, 'test_videos')
 video_names = os.listdir(videos_dir) #! test videolarının isimleri(liste)
 
+def convert_RGB(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+def RGB_color_selection(image):
+
+    img = convert_RGB(image)
+
+    #! Şeritlerdeki beyaz rengi algılamak için alt ve üst değer belirlendi.
+    lower_threshold = np.uint8([200,200,200])
+    upper_threshold = np.uint8([255,255,255])
+    white_mask = cv2.inRange(img, lower_threshold, upper_threshold) #* Beyaz renk için maske elde edildi.
+
+    #! Şeritlerdeki sarı rengi algılamak için alt ve üst değer belirlendi
+    lower_threshold = np.uint8([175,175,0])
+    upper_threshold = np.uint8([255,255,255])
+    yellow_mask = cv2.inRange(img, lower_threshold, upper_threshold)#* Sarı renk için maske elde edildi.
+
+    #! beyaz ve sarı maskedeki görüntüler 'or' ile birleştirildi.
+    mask = cv2.bitwise_or(white_mask, yellow_mask)  
+    #! maske ana görselin üzerine uygulandı.
+    masked_image = cv2.bitwise_and(img, img, mask=mask)
+
+    return masked_image
+
 capture = cv2.VideoCapture(os.path.join(videos_dir, video_names[0]))
+
 
 while True:
 
     _, frame = capture.read()
 
-    cv2.imshow('Screen', frame)
+    cv2.imshow('Screen', RGB_color_selection(frame))
 
     if cv2.waitKey(50)&0xFF == ord('q'):
         break
