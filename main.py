dc26004f329655c9fe1ce4ab11351e30d7eb8fef
EmirKIRAN <emirr.kirann@gmail.com>
2021-06-02 22:06:20 +0300

    added HLS color space selection func.

--- main.py
+++ main.py
@@ -56,6 +56,30 @@ def HSV_color_selection(image):
 
     return masked_image
 
+def convert_HLS(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
+
+def HLS_color_selection(image):
+
+    img = convert_HLS(image)
+
+    #! HLS renk uzayında beyaz renkte bulunan şeritleri tespit edebilmek için renk aralıklarını belirliyoruz.
+    lower_thresholding = np.uint8([0,200,0])
+    upper_thresholding = np.uint8([255,255,255])
+    white_mask = cv2.inRange(img, lower_thresholding, upper_thresholding)
+
+    #! HSL renk uzayında sarı renkte bulunan şeritleri tespit edebilmek için renk aralıklarını belirliyoruz.
+    lower_thresholding = np.uint8([10,0,100])
+    upper_thresholding = np.uint8([40,255,255])
+    yellow_mask = cv2.inRange(img, lower_thresholding, upper_thresholding)
+
+    #! Hem sarı hem de beyaz şeritleri aynı anda tanıyabilmek için iki maskeyi kombinliyoruz(or gate)
+    mask = cv2.bitwise_or(white_mask, yellow_mask)
+    #! Elde ettiğimiz maskeyi tüm görsele uygulayarak elde etmek istediğimiz görseli elde ediyoruz
+    masked_image = cv2.bitwise_and(img, img, mask=mask)
+
+    return masked_image
+    
 capture = cv2.VideoCapture(os.path.join(videos_dir, video_names[0]))
 
 
@@ -63,8 +87,12 @@ while True:
 
     _, frame = capture.read()
 
-    cv2.imshow('Screen 1', RGB_color_selection(frame))
-    cv2.imshow('Screen 2', HSV_color_selection(frame))
+    #cv2.imshow('Screen 1', RGB_color_selection(frame))
+    #cv2.imshow('Screen 2', HSV_color_selection(frame))
+    cv2.imshow('Screen 3', HLS_color_selection(frame))
+    #! En uygun renk uzayının HSL olduğunu belirledik. Artık diğer işlemlere bu renk uzayı ile devam edeceğiz.
+    #! Dilerseniz diğer renk uzaylarını da deneyerek aradaki farkı gözlemleyebilirsiniz.
+    
 
     if cv2.waitKey(50)&0xFF == ord('q'):
         break
